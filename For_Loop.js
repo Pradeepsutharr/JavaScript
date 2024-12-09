e8ef525c75ac068ee0bb3937f5fff5cd77c27d74
pradeepsutharr <psuthar318@gmail.com>
2024-12-09 10:22:58 +0530

    js loops

---
+++ For_Loop.js
@@ -0,0 +1,29 @@
+// //multiply each number from array by 2 and store in variable
+
+// let storedNums = [];
+// let nums = [2, 4, 6, 8];
+
+// //   for(initialization, condition, expression/increament){
+// //     code whatever we want to perform
+// //    }
+
+// for (let index = 0; index < nums.length; index++) {
+//   storedNums.push(nums[index] * 2);
+// }
+// console.log(storedNums); //output => [4,8,12,16]  each number from will be multiplied by 2
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// use for loop and loop over the array and stop the loop when it get value pen and store the values in a new array
+let fruits = ["apple", "banana", "pineapple", "cake", "pen", "grapes"];
+let storedItem = [];
+
+for (let i = 0; i < fruits.length; i++) {
+  if (fruits[i] === "pen") {
+    console.log(`item is => ${fruits[i]} => thats why loop has been stopped`);
+    break;
+  }
+  storedItem.push(fruits[i]);
+  console.log(`item is => ${fruits[i]}`);
+}
+console.log(storedItem);
---
+++ While_Loop.js
@@ -0,0 +1,45 @@
+// while loop that calculate th sum of all numbers from 1 to 5 and store in a variable
+// 1+2+3+4+5
+
+// let sum = 0;
+// let count = 1;
+
+// while (count <= 5) {
+//   sum = sum + 1;
+//   count++;
+// }
+// console.log(sum); // output => 6 which is wrong
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// while (count <= 5) {
+//   sum = sum + count;
+//   count++;
+// }
+// console.log(sum); //output => 15 which is correct
+
+// -----------------------------------------------------------------------------------------------------------------
+// print table of any number
+
+// let num = 5;
+// let counter = 1;
+
+// while (counter <= 10) {
+//   let result = num * counter;
+//   console.log(`${num} X ${counter} = ${result}`);
+//   counter++;
+//   // console.log(result);
+// }
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// create a countdown from 5 to 1 and store in ana array
+
+// let num = 5;
+// let countdown = [];
+// while (num > 0) {
+//   countdown.push(num);
+//   console.log(` stored ${num} in countdown ${countdown}`);
+//   num--;
+// }
+// console.log(countdown); //output => [5,4,3,2,1]
